b19cd451efbab4bb4f62ceccc6debeb93e3e3c16
Vinh-Thuyen <ntvthuyen@gmail.com>
2019-08-31 11:14:54 +0700

    fix login and sign up

--- lib/login.dart
+++ lib/login.dart
@@ -6,6 +6,7 @@ import 'Customize/CustomeIcon.dart';
 import 'signup.dart';
 import 'sign_in.dart';
 import 'main_screen.dart';
+import 'package:flutter/services.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -14,6 +15,14 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String email = "", password = "";
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   Widget horizontalLine() => Padding(
         padding: EdgeInsets.symmetric(horizontal: 16.0),
         child: Container(
@@ -29,48 +38,50 @@ class _MyAppState extends State<MyApp> {
         ScreenUtil(width: 750, height: 1334, allowFontScaling: true);
     return new Scaffold(
         backgroundColor: Colors.white,
-        resizeToAvoidBottomPadding: true,
+        resizeToAvoidBottomPadding: false,
         body: Stack(
           fit: StackFit.expand,
           children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
+            Stack(
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 20.0),
+                Positioned(
+                  top: 0,
                   child: Image.asset("assets/images/main_background.jpg"),
                 ),
-                Expanded(
-                  child: Container(),
-                ),
-                Image.asset("assets/images/image_02.png")
+                Positioned(
+                    bottom: 0, child: Image.asset("assets/images/image_02.png"))
               ],
             ),
             SingleChildScrollView(
               child: Padding(
-                padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 60),
+                padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 40),
                 child: Column(
                   children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Image.asset(
-                          "assets/images/logo.png",
-                          width: ScreenUtil.getInstance().setWidth(110),
-                          height: ScreenUtil.getInstance().setHeight(110),
-                        ),
-                        Text("LADYBUG",
-                            style: TextStyle(
-                                fontSize: ScreenUtil.getInstance().setSp(46),
-                                letterSpacing: .6,
-                                fontWeight: FontWeight.bold))
-                      ],
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                      child: Row(
+                        children: <Widget>[
+                          Image.asset(
+                            "assets/images/logo.png",
+                            width: ScreenUtil.getInstance().setWidth(110),
+                            height: ScreenUtil.getInstance().setHeight(110),
+                          ),
+                          Text("LADYBUG",
+                              style: TextStyle(
+                                  fontSize: ScreenUtil.getInstance().setSp(46),
+                                  letterSpacing: .6,
+                                  fontWeight: FontWeight.bold))
+                        ],
+                      ),
                     ),
+                    /*
                     SizedBox(
-                      height: ScreenUtil.getInstance().setHeight(70),
-                    ),
+                      height: ScreenUtil.getInstance().setHeight(40),//70
+                    ),*/
                     Container(
                       width: double.infinity,
-                      height: ScreenUtil.getInstance().setHeight(500),
+                      //height: MainAxisSize.max,
+                      //height: ScreenUtil.getInstance().setHeight(500),
                       decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(8.0),
@@ -94,7 +105,7 @@ class _MyAppState extends State<MyApp> {
                               "Login",
                               style: TextStyle(
                                   fontFamily: "Poppins-Bold",
-                                  fontSize: ScreenUtil.getInstance().setSp(45),
+                                  fontSize: ScreenUtil.getInstance().setSp(40),
                                   letterSpacing: .6),
                             ),
                             SizedBox(
@@ -142,155 +153,171 @@ class _MyAppState extends State<MyApp> {
                             SizedBox(
                               height: ScreenUtil.getInstance().setHeight(35),
                             ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: <Widget>[
-                                Text(
-                                  "Forgot Password?",
-                                  style: TextStyle(
-                                      fontFamily: "Poppins-Medium",
-                                      color: Colors.orange,
-                                      fontSize:
-                                          ScreenUtil.getInstance().setSp(28)),
-                                )
-                              ],
-                            ),
+                            Padding(
+                                padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: <Widget>[
+                                    Text(
+                                      "Forgot Password?",
+                                      style: TextStyle(
+                                          fontFamily: "Poppins-Medium",
+                                          color: Colors.orange,
+                                          fontSize: ScreenUtil.getInstance()
+                                              .setSp(28)),
+                                    )
+                                  ],
+                                )),
                           ],
                         ),
                       ),
                     ),
+                    /*
                     SizedBox(
                       height: ScreenUtil.getInstance().setHeight(40),
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          InkWell(
-                            child: Container(
-                              width: ScreenUtil.getInstance().setWidth(330),
-                              height: ScreenUtil.getInstance().setHeight(100),
-                              decoration: BoxDecoration(
-                                  gradient: LinearGradient(colors: [
-                                    Color(0xfff12711),
-                                    Color(0xfff5af19)
-                                  ]),
-                                  borderRadius: BorderRadius.circular(6.0),
-                                  boxShadow: [
-                                    BoxShadow(
-                                        color:
-                                            Color(0xfff5af19).withOpacity(.3),
-                                        offset: Offset(0.0, 8.0),
-                                        blurRadius: 8.0)
-                                  ]),
-                              child: Material(
-                                color: Colors.transparent,
-                                child: InkWell(
-                                  onTap: () {
-                                    signInWithEmail(this.email, this.password);
-                                  },
-                                  child: Center(
-                                    child: Text(
-                                      "SIGNIN",
-                                      style: TextStyle(
-                                          fontFamily: "Poppins-Bold",
-                                          color: Colors.white,
-                                          fontSize: 18,
-                                          letterSpacing: 1.0),
+                    ),*/
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              InkWell(
+                                child: Container(
+                                  width: ScreenUtil.getInstance().setWidth(330),
+                                  height:
+                                      ScreenUtil.getInstance().setHeight(100),
+                                  decoration: BoxDecoration(
+                                      gradient: LinearGradient(colors: [
+                                        Color(0xfff12711),
+                                        Color(0xfff5af19)
+                                      ]),
+                                      borderRadius: BorderRadius.circular(6.0),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color: Color(0xfff5af19)
+                                                .withOpacity(.3),
+                                            offset: Offset(0.0, 8.0),
+                                            blurRadius: 8.0)
+                                      ]),
+                                  child: Material(
+                                    color: Colors.transparent,
+                                    child: InkWell(
+                                      onTap: () {
+                                        signInWithEmail(
+                                            this.email, this.password);
+                                      },
+                                      child: Center(
+                                        child: Text(
+                                          "SIGN IN",
+                                          style: TextStyle(
+                                              fontFamily: "Poppins-Bold",
+                                              color: Colors.white,
+                                              fontSize: 15,
+                                              letterSpacing: 1.0),
+                                        ),
+                                      ),
                                     ),
                                   ),
                                 ),
                               ),
-                            ),
+                            ])),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          horizontalLine(),
+                          Text(
+                            "Social Login",
+                            style: TextStyle(
+                                fontSize: 16.0, fontFamily: "Poppins-Medium"),
                           ),
-                        ]),
-                    SizedBox(
-                      height: ScreenUtil.getInstance().setHeight(40),
+                          horizontalLine()
+                        ],
+                      ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        horizontalLine(),
-                        Text(
-                          "Social Login",
-                          style: TextStyle(
-                              fontSize: 16.0, fontFamily: "Poppins-Medium"),
-                        ),
-                        horizontalLine()
-                      ],
-                    ),
-                    SizedBox(
-                      height: ScreenUtil.getInstance().setHeight(40),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        SocialIcon(
-                          colors: Color(0xfff5af19),
-                          iconData: CustomIcons.facebook,
-                          onPressed: () {},
-                        ),
-                        SocialIcon(
-                          colors: Color(0xfff5af19),
-                          iconData: CustomIcons.googlePlus,
-                          onPressed: () {
-                            signInWithGoogle().whenComplete(() {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) {
-                                    return MainScreen();
+                    //child: Stack(children: <Widget>[
+                    /*Image.asset(
+                        "assets/images/image_02.png",
+                        // height: double.infinity,
+                        width: double.infinity,
+                      ),*/
+                    Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                SocialIcon(
+                                  colors: Color(0xfff5af19),
+                                  iconData: CustomIcons.facebook,
+                                  onPressed: () {},
+                                ),
+                                SocialIcon(
+                                  colors: Color(0xfff5af19),
+                                  iconData: CustomIcons.googlePlus,
+                                  onPressed: () {
+                                    signInWithGoogle().whenComplete(() {
+                                      Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                          builder: (context) {
+                                            return MainScreen();
+                                          },
+                                        ),
+                                      );
+                                    });
                                   },
                                 ),
-                              );
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: ScreenUtil.getInstance().setHeight(30),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          "New User? ",
-                          style: TextStyle(fontFamily: "Poppins-Medium"),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.of(context)
-                                .push(MaterialPageRoute(builder: (context) {
-                              return SignUp();
-                            }));
-                          },
-                          child: Text("SignUp",
-                              style: TextStyle(
-                                  color: Color(0xfff5af19),
-                                  fontFamily: "Poppins-Bold")),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: ScreenUtil.instance.setHeight(30),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          "Create as an organisation? ",
-                          style: TextStyle(fontFamily: "Poppins-Medium"),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            organisationSignup();
-                          },
-                          child: Text("Click Here",
-                              style: TextStyle(
-                                  color: Color(0xfff5af19),
-                                  fontFamily: "Poppins-Bold")),
-                        )
-                      ],
-                    )
+                              ],
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text("New User?",
+                                  style: TextStyle(
+                                      fontFamily: "Poppins-Medium",
+                                      fontSize:
+                                          ScreenUtil.getInstance().setSp(26))),
+                              InkWell(
+                                onTap: () {
+                                  Navigator.of(context).push(
+                                      MaterialPageRoute(builder: (context) {
+                                    return SignUp();
+                                  }));
+                                },
+                                child: Text("Sign Up",
+                                    style: TextStyle(
+                                        color: Color(0xfff5af19),
+                                        fontFamily: "Poppins-Bold")),
+                              )
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "You are organisation?",
+                                style: TextStyle(
+                                    fontFamily: "Poppins-Medium",
+                                    fontSize:
+                                        ScreenUtil.getInstance().setSp(26)),
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  organisationSignup();
+                                },
+                                child: Text("Click Here",
+                                    style: TextStyle(
+                                        color: Color(0xfff5af19),
+                                        fontFamily: "Poppins-Bold")),
+                              )
+                            ],
+                          )
+                        ])
                   ],
                 ),
               ),
